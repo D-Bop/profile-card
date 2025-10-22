6e79993f60c578c5abe01f0d6cb776b7f9a98410
D-Bop <official.og.owolabi@gmail.com>
2025-10-22 12:31:38 +0100

    fixed some edge cases in the form

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -1,64 +1,102 @@
-console.log("console is working now")
-
 const form = document.getElementById("form")
 const name = document.getElementById("name")
 const email = document.querySelector("[data-testid='test-contact-email']")
 const emailError = document.querySelector("[data-testid='test-contact-error-email']")
 const subject = document.getElementById("subject")
-const message = document.getElementById("message")
+
+
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+    const nameError = document.querySelector("[data-testid='test-contact-error-name']");
+    const subjectError = document.querySelector("[data-testid='test-contact-error-subject']");
+    const messageError = document.querySelector("[data-testid='test-contact-error-message']");
 
-    console.log("form submitted")
+    let isValid = true;
+
     if (name.value === "") {
-        const nameError = document.querySelector("[data-testid='test-contact-error-name']");
+        
         nameError.classList.add("errorMessage");
         name.classList.add("errorBorder")
-        nameError.innerHTML = `<p>Please input your name!</p>    `
+        nameError.textContent = "Please input your name!"
         console.log("name is empty")
+        isValid = false;
     }
 
     const emailValue = email.value.trim();
     console.log(`Email Value: ${emailValue}`)
     if (!emailValue){
-        console.log(emailValue)
-        
+        console.log("email is empty")
         emailError.classList.add("errorMessage")
-        emailError.innerHTML = `<p>Email is required</p>`
-        
+        emailError.textContent = "Email is required"
         email.classList.add("errorBorder")
+        isValid = false;
     } else if(!emailRegex.test(emailValue)) {
         emailError.classList.add("errorMessage")
-        emailError.innerHTML = `<p>Please enter a valid email</P>`
+        email.classList.add("errorBorder")
+        emailError.textContent = "Please enter a valid email"
         console.log("Email is not valid")
+        isValid = false;
     }
 
     if (subject.value === "") {
-        const subjectError = document.querySelector("[data-testid='test-contact-error-subject']")
-        subjectError.innerHTML = `<p>Please enter a valid Subject</p>`
+       console.log("subject is empty")
+        subjectError.textContent = "Please enter a valid Subject"
         subjectError.classList.add("errorMessage")
         subject.classList.add("errorBorder")
+        isValid = false;
     }
 
-    if(message.value < 11) {
+    const messageInput = document.getElementById("message")
+    console.log(message) 
+    console.log(messageInput.value)
+
+    if(messageInput.value === "") {
+        console.log(messageInput)
+        console.log("message is empty")
+        messageError.textContent = "Message can not be empty";
+        messageError.classList.add("errorMessage")
+        message.classList.add("errorBorder")
+        isValid = false;
+    } else if (message.value.length < 11) {
         console.log("message is too short")
-        const messageError = document.querySelector("[data-testid='test-contact-error-message'")
-        messageError.innerHTML = `<p>Message must be more than 10 characters</p>`
+        isValid = false;
+        messageError.textContent = "Message should be at least 10 characters"
         messageError.classList.add("errorMessage")
         message.classList.add("errorBorder")
     }
-
-    if(name.value && message.value && email.value && subject.value) {
+    
+    if(isValid) {
         console.log("successs")
         const successMessage = document.querySelector("[data-testid='test-contact-success']")
-        successMessage.innerHTML = `<p>Message Successfully submitted</p>`
+        successMessage.textContent = "Message Successfully submitted"
         successMessage.classList.add("success")
         form.reset()
+        clearErrorMessage()
 
         setTimeout(() => {
             successMessage.classList.add("hidden")
-        }, 3000)
+        }, 7000)
+
+        console.log(message.value)
+    }
+
+    function clearErrorMessage() {
+        nameError.classList.remove("errorMessage")
+        nameError.innerHTML = ``
+        name.classList.remove("errorBorder")
+
+        emailError.classList.remove("errorMessage")
+        emailError.innerHTML = ``
+        email.classList.remove("errorBorder")
+
+        subjectError.classList.remove("errorMessage")
+        subjectError.innerHTML = ``
+        subject.classList.remove("errorBorder")
+
+        messageError.classList.remove("errorMessage")
+        messageError.innerHTML = ``
+        message.classList.remove("errorBorder")
     }
 })
